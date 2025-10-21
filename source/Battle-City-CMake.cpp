eae976d7974d422f8757ed3deca62991d5d9fe4b
ArsDari <darizhapov.arsalan@gmail.com>
2025-10-21 12:00:42 +0300

    Start

---
+++ source/Battle-City-CMake.cpp
@@ -0,0 +1,12 @@
+﻿// Battle-City-CMake.cpp: определяет точку входа для приложения.
+//
+
+#include "Battle-City-CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
