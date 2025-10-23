d5be2da25a81920fe3667c7deed4c4dfad2e27f6
ArsDari <darizhapov.arsalan@gmail.com>
2025-10-23 12:35:13 +0300

    Update. Cannot be compiled right now.

---
+++ include/Application.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ResourceHolder.hpp"
+#include "ResourceIdentifiers.hpp"
+#include "Player.hpp"
+#include "StateStack.hpp"
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+
+class Application
+{
+public:
+	Application();
+	void run();
+private:
+	void processInput();
+	void update(sf::Time deltaTime);
+	void render();
+	void updateStatistics(sf::Time deltaTime);
+	void loadTextures();
+	void createPlayers() const;
+	void registerStates();
+	static const sf::Time TimePerFrame;
+	sf::RenderWindow mWindow;
+	TextureHolder mTextures;
+	FontHolder mFonts;
+	State::Players mPlayers;
+	State::Variables mVariables;
+	StateStack mStateStack;
+	sf::Text mStatisticsText;
+	sf::Time mStatisticsUpdateTime;
+	std::size_t mStatisticsNumFrames;
+};
